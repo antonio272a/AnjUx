9793e101b63c91de64134c6641979b6e7a3c3594
Antonio Felipe da Silva de Souza <Antoniofelipe2001@hotmail.com>
2024-05-25 17:48:09 -0300

    up

--- Server/Services/IBGEService.cs
+++ Server/Services/IBGEService.cs
@@ -1,9 +1,26 @@
-﻿namespace AnjUx.Server.Services
+﻿using AnjUx.Shared.Models.Data;
+using System.Text.Json;
+
+namespace AnjUx.Server.Services
 {
     public class IBGEService
     {
         private readonly string baseUrl = "https://servicodados.ibge.gov.br/api/";
 
+        public async Task<List<Municipio>> GetMunicipios()
+        {
+            using HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync($"{baseUrl}v1/localidades/municipios");
 
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<Municipio>>(content)!;
+            }
+
+            string erro = await response.Content.ReadAsStringAsync();
+
+            throw new Exception($"Erro ao buscar municípios: {erro}");
+        }
     }
 }
